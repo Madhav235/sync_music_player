12679f0c012d5e10ec086a21bd29a9c0a92d1f0a
Madhav Agarwal <madhavagarwal369@gmail.com>
2026-03-27 22:08:44 +0530

    dynamically add suggestions to the suggestions modal

--- home.js
+++ home.js
@@ -1,31 +1,11 @@
 import { getSpotifyToken ,api} from "./spotifyToken.js";
 
-// export async function searchSpotify(query) {
-//     console.log("Search query started");
-//     const token = await getSpotifyToken(); // your existing function
-
-//     const response = await fetch(
-//         `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=track&limit=1`,
-//         {
-//             headers: {
-//                 Authorization: `Bearer ${token}`
-//             }
-//         }
-//     );
-
-//     const data = await response.json();
-//     return data;
-// }
-
-// searchSpotify("Ed sheeran")
-// .then(data => console.log(data))
-
 // Search bar Functionality
 
 // Search bar select, search Modal
 
 const searchInput = document.querySelector("#searchInput");
-const searchModal = document.querySelector("#searchModal");
+const searchModal = document.querySelector("#jsSearchModal");
 const searchBar = document.querySelector(".searchBar");
 
 // adding click event on search input and search modal toggle logic
@@ -45,4 +25,83 @@ window.addEventListener("click",() => {
     console.log("Window clicked");
     searchModal.classList.remove("searchModalActive");
     searchBar.classList.remove("searchBarActive");
+});
+
+// sending user search query to the spotify api
+
+export async function searchSpotify(query) {
+    console.log("Search query started");
+    console.log(query);
+    const token = await getSpotifyToken();
+
+    const response = await fetch(
+        `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=track&limit=1`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+    );
+
+    const data = await response.json();
+    return data;
+}
+
+// Making search modal suggestions dynamically
+
+function createSearchModalSuggestion(){
+    console.log("Dynamic Modal suggestion creation started");
+
+    // selecting parent search modal
+    const searchModal = document.querySelector("#jsSearchModal");
+
+    // creating elements dynamically
+
+    const searchModalItem = document.createElement("div");
+    searchModalItem.classList.add("searchModalItem");
+
+    const songCover = document.createElement("div");
+    songCover.classList.add("songCover");
+
+    const songCoverLayout = document.createElement("div");
+    songCoverLayout.classList.add("songCoverLayout");
+
+    const imageSource = document.createElement("img");
+    imageSource.classList.add("imageSource");
+
+    const songInformation = document.createElement("div");
+    songInformation.classList.add("songInformation");
+
+    const songName = document.createElement("div");
+    songName.classList.add("songName");
+
+    const songArtist = document.createElement("div");
+    songArtist.classList.add("songArtist");
+
+    songCoverLayout.appendChild(imageSource);
+    songCover.appendChild(songCoverLayout);
+
+    searchModalItem.appendChild(songCover);
+
+    songInformation.appendChild(songName);
+    songInformation.appendChild(songArtist);
+
+    searchModalItem.appendChild(songInformation);
+
+    searchModal.appendChild(searchModalItem);
+}
+
+// Implementing spotify query on user search by clicking search icon
+
+// Taking user input
+
+const searchIcon = document.querySelector("#jsSearchIcon");
+let searchData = null;
+
+searchIcon.addEventListener("click",(e) => {
+    e.stopPropagation();
+    console.log("search icon clicked");
+    searchData = searchSpotify(`${searchInput.value}`);
+    // searchData.then(data => console.log(data.tracks.items[0].artists[0].name));
+    // createSearchModalSuggestion();
 });
--- spotifyToken.js
+++ spotifyToken.js
@@ -3,31 +3,41 @@ import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } from "./config.js";
 
 let accessToken = null;
 let tokenExpiry = 0;
+let tokenPromise = null;
 
 export async function getSpotifyToken() {
-  if (accessToken && Date.now() < tokenExpiry) {
-    console.log("Using cached Token");
-    return accessToken;
-  }
+    if (accessToken && Date.now() < tokenExpiry) {
+        console.log("Using cached Token");
+        return accessToken;
+    }
 
-  const response = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",
-    headers: {
-      Authorization:
-        "Basic " + btoa(SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET),
-        "Content-Type": "application/x-www-form-urlencoded"
-    },
-    body: new URLSearchParams({
-      grant_type: "client_credentials"
+    if (tokenPromise) {
+        console.log("Waiting for existing token request");
+        return tokenPromise;
+    }
+
+    tokenPromise = fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            Authorization:
+                "Basic " + btoa(SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET),
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+            grant_type: "client_credentials",
+        }),
     })
-  });
+        .then(res => res.json())
+        .then(data => {
+            accessToken = data.access_token;
+            tokenExpiry = Date.now() + (data.expires_in - 60) * 1000;
 
-  const data = await response.json();
+            tokenPromise = null; // reset
 
-  accessToken = data.access_token;
-  tokenExpiry = Date.now() + (data.expires_in-60) * 1000;
+            return accessToken;
+        });
 
-  return accessToken;
+    return tokenPromise;
 }
 
 export async function api() {
