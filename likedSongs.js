dfa7fe4ebd9e01fcec742b83fc751ed02a57c64a
Madhav Agarwal <madhavagarwal369@gmail.com>
2026-04-05 23:21:57 +0530

    change 2

--- likedSongs.js
+++ likedSongs.js
@@ -86,3 +86,182 @@ function getAdditionDate(data) {
   let day = date.getDate();
   let month = date.getMonth();
   let year = date.getFullYear();
+
+  let monthFinal = null;
+
+  switch (month) {
+    case 0:
+      monthFinal = "Jan";
+      break;
+    case 1:
+      monthFinal = "Feb";
+      break;
+    case 2:
+      monthFinal = "Mar";
+      break;
+    case 3:
+      monthFinal = "Apr";
+      break;
+    case 4:
+      monthFinal = "May";
+      break;
+    case 5:
+      monthFinal = "Jun";
+      break;
+    case 6:
+      monthFinal = "Jul";
+      break;
+    case 7:
+      monthFinal = "Aug";
+      break;
+    case 8:
+      monthFinal = "Sep";
+      break;
+    case 9:
+      monthFinal = "Oct";
+      break;
+    case 10:
+      monthFinal = "Nov";
+      break;
+    case 11:
+      monthFinal = "Dec";
+      break;
+  }
+
+  let addDate = `${day} ${monthFinal} ${year}`;
+  data.addDate = addDate;
+}
+
+// get total duration
+
+function setTotalDuration(data) {
+  let duration = player.getDuration();
+  let [minutes, seconds] = formatInMinuteSeconds(duration);
+  let songDuration = `${minutes}:${seconds}`;
+  data.Duration = songDuration;
+}
+
+// adding to Local Storage
+
+function addToLocalStorage(songData) {
+  // setting to local storage
+  localStorageData.push(songData);
+  localStorage.setItem("likedSongsData", JSON.stringify(localStorageData));
+}
+
+// rendering new songs
+
+function renderLikedSongItem(songData, idx) {
+  console.log("Liked song rendering started");
+
+  // Dynamically creating the item component
+
+  const likedSongItem = document.createElement("div");
+  likedSongItem.classList.add("likedSongItem");
+
+  const serialNumber = document.createElement("div");
+  serialNumber.classList.add("serialNumber");
+
+  const likedSongCover = document.createElement("div");
+  likedSongCover.classList.add("likedSongCover");
+
+  const likedSongCoverLayout = document.createElement("div");
+  likedSongCoverLayout.classList.add("likedSongCoverLayout");
+
+  const likedSongCoverSource = document.createElement("img");
+  likedSongCoverSource.classList.add("likedSongCoverSource");
+
+  const likedSongDescription = document.createElement("div");
+  likedSongDescription.classList.add("likedSongDescription");
+
+  const likedSongInfo = document.createElement("div");
+  likedSongInfo.classList.add("likedSongInfo");
+
+  const likedSongName = document.createElement("div");
+  likedSongName.classList.add("likedSongName");
+
+  const likedSongArtist = document.createElement("div");
+  likedSongArtist.classList.add("likedSongArtist");
+
+  const likedSongAlbum = document.createElement("div");
+  likedSongAlbum.classList.add("likedSongAlbum");
+
+  const likedSongDateAdded = document.createElement("div");
+  likedSongDateAdded.classList.add("likedSongDateAdded");
+
+  const likedSongDuration = document.createElement("div");
+  likedSongDuration.classList.add("likedSongDuration");
+
+  // Putting all element in structure
+
+  likedSongCoverLayout.appendChild(likedSongCoverSource);
+  likedSongCover.appendChild(likedSongCoverLayout);
+
+  likedSongInfo.appendChild(likedSongName);
+  likedSongInfo.appendChild(likedSongArtist);
+
+  likedSongDescription.appendChild(likedSongInfo);
+  likedSongDescription.appendChild(likedSongAlbum);
+  likedSongDescription.appendChild(likedSongDateAdded);
+  likedSongDescription.appendChild(likedSongDuration);
+
+  likedSongItem.appendChild(serialNumber);
+  likedSongItem.appendChild(likedSongCover);
+  likedSongItem.appendChild(likedSongDescription);
+
+  // Setting up the song data
+
+  likedSongName.innerHTML = songData.Song;
+  likedSongArtist.innerHTML = songData.Artist;
+  likedSongCoverSource.setAttribute("src", songData.coverUrl);
+  likedSongAlbum.innerHTML = songData.Album;
+  serialNumber.innerHTML = `${idx + 1}.`;
+  likedSongDateAdded.innerHTML = `${songData.addDate}`;
+  likedSongDuration.innerHTML = `${songData.Duration}`;
+
+  // Rendering final item to the song container
+
+  const likedSongsContainer = document.querySelector("#jsLikedSongsContainer");
+  console.log(likedSongsContainer);
+  songData.element = likedSongItem;
+  likedSongsContainer.appendChild(likedSongItem);
+}
+
+updateLikedSongContainer();
+
+// playing the selected liked song on the player
+
+function addEventToAllLikedSongs() {
+  likedSongContainer.forEach((item) => {
+    item.element.addEventListener("click", (e) => {
+      e.stopPropagation();
+      let videoId = item.songId;
+      showUserSelectedSong(item.coverUrl, item.Song, item.Artist);
+      playSongOnPLayer(null, null, videoId);
+    });
+  });
+}
+
+// opening liked songs page on clicking the liked songs
+
+const likedSongItem = document.querySelector("#jsLikedSongsItem");
+
+likedSongItem.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log("item clicked");
+  const likedSong = document.querySelector("#jsLikedSongs");
+  likedSong.classList.add("active");
+
+  const mainPanel = document.querySelector("#jsMainPanel");
+  mainPanel.classList.remove("active");
+});
+
+const homeButton = document.querySelector("#jsHomeButton");
+
+homeButton.addEventListener("click", (e) => {
+  const likedSong = document.querySelector("#jsLikedSongs");
+  likedSong.classList.remove("active");
+
+  const mainPanel = document.querySelector("#jsMainPanel");
+  mainPanel.classList.add("active");
+});
